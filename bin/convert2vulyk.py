623c1f62c377180b72d54f8d1dbb4969b99de3de
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2022-01-24 00:21:58 +0200

    CR changes, fix for the help texts for sub-commands

--- bin/convert2vulyk.py
+++ bin/convert2vulyk.py
@@ -27,7 +27,6 @@ class StanzaNER:
     def tag_text(self, txt):
         doc = self.ner(txt)
 
-        tok_i = 1
         brat_str = ""
 
         for tok_i, ent in enumerate(doc.ents):
@@ -130,14 +129,14 @@ def convert_bsf_2_vulyk(text: str, bsf_markup: str) -> dict:
     return vulyk
 
 
-def convert(args):
-    for text in map(pathlib.Path, glob.glob(args.text_files)):
+def convert(text_files, ignore_annotations, ann_autodiscovery):
+    for text in map(pathlib.Path, glob.glob(text_files)):
         log.info(f"Found text file {text}, parsing it")
 
         markup = ""
 
-        if not args.ignore_annotations:
-            if args.ann_autodiscovery == "append":
+        if not ignore_annotations:
+            if ann_autodiscovery == "append":
                 ann = text.with_name(text.name + ".ann")
             else:
                 ann = text.with_suffix(".ann")
@@ -151,13 +150,21 @@ def convert(args):
         print(json.dumps(vulyk_obj, ensure_ascii=False, sort_keys=True))
 
 
-def tag(args):
-    if args.ner_framework == "stanza":
-        model = StanzaNER(args.ner_model)
-    elif args.ner_framework == "spacy":
-        model = SpacyNER(args.ner_model)
+def convert_command(args):
+    return convert(
+        text_files=args.text_files,
+        ignore_annotations=args.ignore_annotations,
+        ann_autodiscovery=args.ann_autodiscovery,
+    )
 
-    for text in map(pathlib.Path, glob.glob(args.text_files)):
+
+def tag(text_files, ner_framework, ner_model):
+    if ner_framework == "stanza":
+        model = StanzaNER(ner_model)
+    elif ner_framework == "spacy":
+        model = SpacyNER(ner_model)
+
+    for text in map(pathlib.Path, glob.glob(text_files)):
         log.info(f"Found text file {text}, tagging it")
 
         token_list = tokenize_text(text.read_text())
@@ -173,15 +180,67 @@ def tag(args):
         print(json.dumps(vulyk_obj, ensure_ascii=False, sort_keys=True))
 
 
+def tag_command(args):
+    return tag(args.text_files, args.ner_framework, args.ner_model)
+
+
 if __name__ == "__main__":
     logging.basicConfig()
 
     parser = argparse.ArgumentParser(
-        description="Convert tokenized pre-annotated text or tag generic text to json file supported by Vulyk. "
-        "Data file can be supplied via cmd line arguments "
-        "or you can pipe data in and out of this script like:"
-        "`cat file.txt | python3 convert2vulyk.py tag >> save_to_file.json`"
+        description="Convert tokenized pre-annotated texts or tag generic texts to jsonlines file supported by Vulyk. \n"
+        "Data files supplied via glob-like cmd line arguments. Output goes directly to stdout. \n"
+        "Script has two modes: convert, to convert texts (with optional annotations) "
+        "and tag, to tag provided tokenized texts using one of supported frameworks. "
+        "Please check help texts for the respecting commands for details on parameters."
     )
+
+    subparsers = parser.add_subparsers(dest="cmd", help="Available commands")
+
+    convert_parser = subparsers.add_parser(
+        "convert",
+        help="Convert tokenized texts (with optional annotations in a separate files) to a Vulyk format "
+        "To make it parse all text files together with annotations you should use something like this "
+        "`python bin/convert2vulyk.py convert -v 'corpus/*.txt'`. Annotation auto-discovery is ruled "
+        "by the setting --ann_autodiscovery, which might append or replace .ann extension in the name of the text file "
+        "To disable annotation auto-discovery use --ignore_annotations flag",
+    )
+
+    convert_parser.set_defaults(func=convert_command)
+    convert_parser.add_argument(
+        "--ann_autodiscovery",
+        default="replace",
+        choices=("append", "replace"),
+        help="How to find *.ann files: by appending or replacing the extension",
+    )
+    convert_parser.add_argument(
+        "--ignore_annotations", default=False, action="store_true", help="Do not try to load *.ann files"
+    )
+
+    tag_parser = subparsers.add_parser(
+        "tag",
+        help="Tag given files using one of frameworks/models specified in params. "
+        "Currently the script supports Stanza and Spacy frameworks. To use the script in this mode you should install "
+        "additional packages from extra_requirements.txt. "
+        "Make sure that you have enough RAM and some patience",
+    )
+
+    tag_parser.add_argument(
+        "--ner_framework",
+        choices=("stanza", "spacy"),
+        default="stanza",
+        help="Which framework to use for the tagging",
+    )
+
+    tag_parser.add_argument(
+        "--ner_model",
+        default="uk",
+        help="Which model to use. Stanza has pre-built model `uk` for ukrainian language. "
+        "For Spacy you might specify pre-built model (you should run `python -m spacy download model_name` first) "
+        "or provide a path to the directory with the model",
+    )
+
+    tag_parser.set_defaults(func=tag_command)
 
     parser.add_argument(
         "text_files",
@@ -207,43 +266,6 @@ if __name__ == "__main__":
         dest="loglevel",
         const=logging.INFO,
     )
-
-    subparsers = parser.add_subparsers(help="Available commands")
-
-    convert_parser = subparsers.add_parser(
-        "convert",
-        help="Convert tokenized texts (with optional annotations in a separate files) to a Vulyk format",
-    )
-
-    convert_parser.set_defaults(func=convert)
-    convert_parser.add_argument(
-        "--ann_autodiscovery",
-        default="replace",
-        choices=("append", "replace"),
-        help="How to find *.ann files: by appending or replacing the extension",
-    )
-    convert_parser.add_argument(
-        "--ignore_annotations", default=False, action="store_true", help="Do not try to load *.ann files"
-    )
-
-    tag_parser = subparsers.add_parser("tag", help="Tag given files using one of frameworks/models specified in params")
-
-    tag_parser.add_argument(
-        "--ner_framework",
-        choices=("stanza", "spacy"),
-        default="stanza",
-        help="Which framework to use for the tagging",
-    )
-
-    tag_parser.add_argument(
-        "--ner_model",
-        default="uk",
-        help="Which model to use. Stanza has pre-built model `uk` for ukrainian language. "
-        "For Spacy you might specify pre-built model (you should run `python -m spacy download model_name` first) "
-        "or provide a path to the directory with the model",
-    )
-
-    tag_parser.set_defaults(func=tag)
 
     args = parser.parse_args()
 
